cc0caf828bff1ecccefdcb18dfd1fcb809df7790
Clayton <clayton@claytonlz.com>
2013-02-27 23:06:25 -0700

    Actually Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scoreopenface::Application.config.secret_token = 'c9774acffc4499e07668447a48dfbbf969683d4bc1cd93c4a51a4ddcc9a1b12737c8455a9483f01ac7848631b51c20e2997d9eb90ba5fab61235995fc40dd321'
